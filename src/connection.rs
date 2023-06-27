31181a3dc373b2578bccb097e901440a615df518
Sridhar Ratnakumar <srid@srid.ca>
2023-06-27 10:26:22 -0400

    Run `, fmt`

--- src/connection.rs
+++ src/connection.rs
@@ -3,17 +3,21 @@
 use dotenv::var;
 
 pub fn connect() -> redis::Connection {
-  let redis_host = var("REDIS_HOST").expect("missing environment variable REDIS_HOST");
-  let redis_hostport = var("REDIS_HOSTPORT").expect("missing environment variable REDIS_HOSTPORT");
+    let redis_host = var("REDIS_HOST").expect("missing environment variable REDIS_HOST");
+    let redis_hostport =
+        var("REDIS_HOSTPORT").expect("missing environment variable REDIS_HOSTPORT");
 
-  let redis_password = var("REDIS_PASSWORD").unwrap_or_default();
+    let redis_password = var("REDIS_PASSWORD").unwrap_or_default();
 
-  let redis_conn_url = format!("redis://{}@{}:{}", redis_password, redis_host, redis_hostport);
+    let redis_conn_url = format!(
+        "redis://{}@{}:{}",
+        redis_password, redis_host, redis_hostport
+    );
 
-  println!("{}",redis_conn_url);
+    println!("{}", redis_conn_url);
 
-  redis::Client::open(redis_conn_url)
-      .expect("Invalid connection URL")
-      .get_connection()
-      .expect("failed to connect to Redis")
+    redis::Client::open(redis_conn_url)
+        .expect("Invalid connection URL")
+        .get_connection()
+        .expect("failed to connect to Redis")
 }
