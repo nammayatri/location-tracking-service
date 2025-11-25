d095dde2323e66cb0501c14b010bf73505092e48
Anirban Das <anirbandas0206@gmail.com>
2025-11-25 13:20:00 +0530

    lts_hotpush: Wimp_v2_changes and variants (#202)

--- crates/location_tracking_service/src/common/types.rs
+++ crates/location_tracking_service/src/common/types.rs
@@ -192,6 +192,15 @@ pub enum VehicleType {
     #[strum(serialize = "VIP_OFFICER")]
     #[serde(rename = "VIP_OFFICER")]
     VipOfficer,
+    #[strum(serialize = "BIKE_PLUS")]
+    #[serde(rename = "BIKE_PLUS")]
+    BikePlus,
+    #[strum(serialize = "AC_PRIORITY")]
+    #[serde(rename = "AC_PRIORITY")]
+    AcPriority,
+    #[strum(serialize = "E_RICKSHAW")]
+    #[serde(rename = "E_RICKSHAW")]
+    ERickShaw,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, Display, PartialEq)]
@@ -213,6 +222,19 @@ pub enum RideInfo {
         pickup_location: Point,
         min_distance_between_two_points: Option<i32>,
         ride_stops: Option<Vec<Point>>,
+    },
+    #[serde(rename_all = "camelCase")]
+    Pilot {
+        destination: Point,
+        driver_name: Option<String>,
+        duty_type: Option<String>,
+        end_address: Option<String>,
+        group_id: Option<String>,
+        pilot_number: String,
+        scheduled_trip_time: Option<TimeStamp>,
+        source: Point,
+        start_address: Option<String>,
+        vip_name: Option<String>,
     },
 }
 
