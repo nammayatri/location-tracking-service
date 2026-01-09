a05c5ecfcb666379d29f573f3c1d4426ffd03a5b
Vijay Gupta <vijayrauniyar1818@gmail.com>
2026-01-10 00:02:32 +0530

    fix: update route data reading to include GCP_PROJECT environment variable

--- crates/location_tracking_service/src/common/route.rs
+++ crates/location_tracking_service/src/common/route.rs
@@ -39,7 +39,7 @@ pub async fn read_route_data(
     google_api_key: &str,
     force_refresh: bool,
 ) -> Result<FxHashMap<String, Route>, AppError> {
-    if var("DEV").is_ok() {
+    if var("DEV").is_ok() || var("GCP_PROJECT").is_ok() {
         let config_path =
             var("ROUTE_GEO_JSON_CONFIG").unwrap_or_else(|_| "./route_geo_json_config".to_string());
         let geometries = fs::read_dir(&config_path).map_err(|err| {
